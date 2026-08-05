53d81540c7fe14cb8cb54de9a690fb0b0340abb9
ztifff <eduartefitzgerald@gmail.com>
2026-08-05 09:55:12 +0800

    Analytics

--- src/main.tsx
+++ src/main.tsx
@@ -4,8 +4,11 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
 
+import { Analytics } from "@vercel/analytics/react";
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
+    <Analytics />
   </StrictMode>
 );
